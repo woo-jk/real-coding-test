c542f10785c21a3173a0d2dcb49c2c9ee69b2fa8
woo-jk <201702042@o.cnu.ac.kr>
2021-04-28 23:39:48 +0900

    modify

--- src/test/java/ChampionTest.java
+++ src/test/java/ChampionTest.java
@@ -4,6 +4,10 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 public class ChampionTest {
     private List<Champion> championList = new ArrayList<Champion>();
@@ -30,18 +34,22 @@ public class ChampionTest {
     @Test
     public void givenCollectionWhenEmptyCorrect() {
         List<String> emptyList = new ArrayList<>();
+        assertThat(emptyList, empty());
     }
+
 
     //notNullValue 활용한 테스트
     @Test
     public void notNullCheck() {
         String lck = "LCK";
+        assertThat(lck, notNullValue());
     }
 
     //nullValue 활용한 테스트
     @Test
     public void givenStringWhenNullIsCorrect() {
         String lck = null;
+        assertThat(lck, nullValue());
     }
 
     //문자열 관련 테스트 anyOf, containsString, endWith
@@ -51,44 +59,49 @@ public class ChampionTest {
         String sampleString2 = "Player point";
         String startString = "Player";
         String endString = "point";
+        assertThat(sampleString1, anyOf(containsString(startString)));
+        assertThat(sampleString2, anyOf(endsWith(endString)));
     }
 
     //부동소수점 범위 closeTo 테스트
     @Test
     public void testForFloatingPoint() {
-
+        assertThat(1.2, closeTo(1, 0.5));
     }
 
     //anything 테스트
     @Test
     public void shouldNotErrorGetReference() {
-
+        assertThat(championList.get(0), anything());
+        System.out.println(championList.get(0));
     }
 
     //객체 크기 검증 테스트 hasSize
     @Test
     public void shouldChampionCountFive() {
-
+        assertThat(championList, hasSize(5));
     }
 
     //서폿 챔피언은 타릭이어야 한다라는 조건으로 테스트 코드 작성
     @Test
     public void shouldSupportChampionIsTaric() {
-        Champion supportChamp = new Champion("타릭", "바텀");
-
+        Champion supportChamp = new Champion("타릭", "서폿");
+        assertThat("타릭", is(supportChamp.getName()));
+        assertThat("서폿", is(supportChamp.getPosition()));
     }
 
     //hasProperty 활용하여 속성이 포함되어 있는지 테스트
     @Test
     public void shouldHasPropertyPosition() {
-
+        assertThat(championList.get(0), hasProperty("position"));
+        assertThat(championList.get(1), hasProperty("name"));
     }
 
     //hasToString 활용 테스트
     @Test
     public void shouldHaveSomeChampName() {
         List<String> champListNames = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가느", "블라디미르");
-
+        assertThat(champListNames.get(2), hasToString("렉사이"));
     }
 
     //property와 value가 같은지 테스트
@@ -96,13 +109,19 @@ public class ChampionTest {
     public void shouldHaveSamePropertyAndValue() {
         List<String> championNames1 = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가나", "블라디미르");
         List<String> championNames2 = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가나", "블라디미르");
-
+        assertThat(championNames1, samePropertyValuesAs(championNames2));
     }
 
     //탑 챔피언은 다리우스여야 한다라는 조건으로 테스트 코드 작성, stream 활용예
     @Test
     public void shouldTopChampionIsDarius() {
+        Optional<Champion> filteredChampion = championList.stream()
+                .filter(c -> c.getPosition().equals("탑"))
+                .findFirst();
 
+        assertThat(filteredChampion.get().getName(), is("다리우스"));
+        assertTrue(filteredChampion.get().getPosition().equals("탑"));
+        System.out.println("champ ::" + filteredChampion);
     }
 
 }
--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -14,7 +14,7 @@ public class CustomCalculatorTest {
     public void add() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.add(10,15);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result==25);
         System.out.println("result :: " + result);
     }
 
@@ -23,7 +23,7 @@ public class CustomCalculatorTest {
     public void subtract() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.subtract(23,10);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result==13);
         System.out.println("result :: " + result);
     }
 
@@ -32,7 +32,7 @@ public class CustomCalculatorTest {
     public void multiply() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.multiply(5,9);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result==45);
         System.out.println("result :: " + result);
     }
 
@@ -41,7 +41,7 @@ public class CustomCalculatorTest {
     public void divide() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.divide(25,5);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result==5);
         System.out.println("result :: " + result);
     }
 }
